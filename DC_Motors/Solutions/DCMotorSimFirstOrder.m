2537506f8994336ea32862a06fda81116e0c3b18
Varun <vmadabushi3@gatech.edu>
2021-10-25 10:38:25 -0400

    Added first order sim, edited .gitignore, fixed missing variable bug

--- DC_Motors/Solutions/DCMotorSimFirstOrder.m
+++ DC_Motors/Solutions/DCMotorSimFirstOrder.m
@@ -1,29 +1,28 @@
-%% Simulation of DC Motor Systems
+%% Simulation of First Order DC Motor
 % By Varun Madabushi
 % RoboJackets
-% March 2021
+% Oct 2021
 
 close all
 
 %% Parameters
-% taken from Maxon EC 32
 % Load is a wheel with mass 0.35kg and diameter 0.15m spinning freely
 % Only things to be considered are inertia and viscous friction
 J = .1*0.35*(0.15/2)^2; % Kg-m/s^2
 R = 0.573; % Ohms
-L = 0.09e-3; % H
 Ki = 13e-3; % N-m/A
 Kv = 1/Ki; % Rad/s/Volt
-D = 2e-4; % Viscous friction coefficient
+% Viscous friction coefficient is usually unknown but can be set to 0
+D = 2e-8; % Viscous friction coefficient (N-M/(rad/s))
 
 
 %% Initialize Arrays Holding State Variables
-dt = 0.0001;
-t = 0:dt:2; % 10 seconds
+dt = 0.01;
+t = 0:dt:10; % 10 seconds
 theta = zeros(1,length(t));
 omega = zeros(1,length(t));
 current = zeros(1,length(t));
-V = 18; % V
+V = 12; % V
 r = Kv*V;
 
 %% Forward Euler Simulation
@@ -34,26 +33,24 @@ for k = 1:length(t)-1
     % Speed is the integral of position
     thetadot = omega(k);
     
+    % In first order model, ignore inductance. Current is calculated
+    % from Ohm's Law
+    current = (V - (omega(k)/Kv))/R;
+    
     % Acceleration (omegadot)
     % Viscous friction model: friction torque proportional to speed
-    omegadot = (1/J)*(Ki*current(k) - D*omega(k)); % tau_L = D*omega
-    
-    % Current derivative
-    idot = (1/L)*(V - R*current(k) - omega(k)/Kv); 
+    omegadot = (1/J)*(Ki*current - D*omega(k)); % tau_L = D*omega
     
     
     % Forward Euler Method (First Order Taylor Series)
     theta(k+1) = theta(k) + dt*thetadot;
     omega(k+1) = omega(k) + dt*omegadot;
-    current(k+1) = current(k) + dt*idot;
 end
 
 %% Plots
-subplot(3,1,1)
+subplot(2,1,1)
 plot(t,theta)
-subplot(3,1,2)
+subplot(2,1,2)
 hold on
 plot(t,omega)
 plot(t, r*ones(1,length(t)), 'r--')
-subplot(3,1,3)
-plot(t, current)
